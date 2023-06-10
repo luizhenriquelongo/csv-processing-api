89819e14f28ea1dea519a2fefa5da1e44ebf6f60
Luiz Longo <longo.luizh@gmail.com>
2023-06-10 10:02:23 -0300

    feat(app-factory): initialize celery app within app context

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from typing import TYPE_CHECKING, Type
 
 from flask import Flask
 
+from app.extensions.celery import celery_init_app
+
 if TYPE_CHECKING:
     from config import Config
 
@@ -9,4 +11,5 @@ if TYPE_CHECKING:
 def create_app(config: Type["Config"] | str) -> Flask:
     app = Flask(__name__)
     app.config.from_object(config)
+    celery_init_app(app)
     return app
