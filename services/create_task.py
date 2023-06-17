fd9231e3cdd083340c2ea97b3fb2be506f3a87d3
Luiz Longo <longo.luizh@gmail.com>
2023-06-17 16:49:49 -0300

    fix: create task first before call process_cev

--- services/create_task.py
+++ services/create_task.py
@@ -34,9 +34,9 @@ class CreateTaskService:
 
         csv_file.save(input_file_path)
 
-        process_csv.delay(task_id)
-
         task = self.dao.create_new_task(task_id=task_id, input_file_path=str(input_file_path))
+
+        process_csv.delay(task.id)
         response = responses.TaskCreatedResponse(
             task_id=task.id, status=task.status, next=f"/api/v1/tasks/{task.id}/status"
         )
